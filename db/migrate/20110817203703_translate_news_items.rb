afca18d4dd72566afe845ae2890e40f497f7e265
Unknown-Guy <Unknown-Guy@users.noreply.github.com>
2019-10-10 15:01:08 +0300

    Fix tests after refinery upgrade

--- db/migrate/20110817203703_translate_news_items.rb
+++ db/migrate/20110817203703_translate_news_items.rb
@@ -11,7 +11,7 @@ class TranslateNewsItems < ActiveRecord::Migration[4.2]
       t.timestamps null: false
     end
 
-    add_index :refinery_news_translations, :locale, name: :index_refinery_news_translations_on_locale
-    add_index :refinery_news_translations, [:refinery_news_item_id, :locale], name: :index_2mno5zfnvxdf4fd576nsaqrr1lry3cs87sig3hyf, unique: true
+    add_index :refinery_news_item_translations, :locale, name: :index_refinery_news_translations_on_locale
+    add_index :refinery_news_item_translations, [:refinery_news_item_id, :locale], name: :index_2mno5zfnvxdf4fd576nsaqrr1lry3cs87sig3hyf, unique: true
   end
 end
